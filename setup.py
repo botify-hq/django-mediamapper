99ceb8b9540a1f2601a0e71aae4daa399f6b5ebe
ampelmann <thomas.grange@gmail.com>
2012-06-07 12:06:42 +0300

    Update master

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name = "Media Mapper",
     version = "0.1",
-    packages = find_packages(),
     include_package_data = True,
 
     author = "Thomas Grange",
